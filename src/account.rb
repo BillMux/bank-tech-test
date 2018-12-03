9b894c44de863ba2c1f1bc941ee6c3719f0b4c66
Bill Muxworthy <bill_muxworthy@hotmail.com>
2018-12-03 14:01:59 +0000

    prints statement with terminal table

--- src/account.rb
+++ src/account.rb
@@ -1,17 +1,27 @@
+require 'terminal-table'
+require 'time'
+
 class Account
   attr_reader :balance, :statement
 
   def initialize
     @balance = 0
-    @statement = ['Date', 'In', 'Out', 'Balance']
+    @rows = []
+    @head = ['Date', 'In', 'Out', 'Balance']
+    @statement = Terminal::Table.new :headings => @head, :rows => @rows
   end
 
   def deposit(amount)
     @balance += amount
+    @rows << [Time.now, amount, 0, @balance]
   end
 
   def withdraw(amount)
     @balance -= amount
+  end
+
+  def print_statement
+    puts @statement
   end
 
 end
