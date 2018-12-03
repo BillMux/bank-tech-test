90503b77a53049029e5ab6b798001a9f9eb5a952
Bill Muxworthy <bill_muxworthy@hotmail.com>
2018-12-03 18:57:55 +0000

    first feature test

---
+++ spec/features/statement_spec.rb
@@ -0,0 +1,20 @@
+require_relative '../../src/account.rb'
+
+
+# feature 'Statement' do
+#   before :each do
+#     account = Account.new
+#
+#   end
+#
+#   scenario 'contains deposits' do
+#     subject.deposit(100)
+#     expect(subject.print_statement).to eq(
+#      "+--------------------+-----+-----+---------+
+#       | Date               | In  | Out | Balance |
+#       +--------------------+-----+-----+---------+
+#       | 18:49,  3 DEC 2018 | 100 | 0   | 100     |
+#       +--------------------+-----+-----+---------+"
+#     )
+#   end
+# end
