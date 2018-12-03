f88aead8fc1339ea68c5bda50682cdda19ca6959
Bill Muxworthy <bill_muxworthy@hotmail.com>
2018-12-03 12:28:33 +0000

    withdraw and deposit, first two user stories complete

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,12 +1,20 @@
 require_relative '../src/account.rb'
 
-describe 'Account' do
-  before :each do
-    bank = Account.new
+describe Account do
+
+  it 'starts with a balance of zero' do
+    expect(subject.balance).to eq 0
   end
 
   it 'takes a deposit' do
-    bank.deposit(100)
-    expect(bank.balance).to eq 100
+    subject.deposit(100)
+    expect(subject.balance).to eq 100
   end
+
+  it 'gives a withdrawal' do
+    subject.deposit(1000)
+    subject.withdraw(100)
+    expect(subject.balance).to eq 900
+  end
+
 end
