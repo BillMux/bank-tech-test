f9cedc5ec4b69944dfa9d86a76fc434a97636944
Bill Muxworthy <bill_muxworthy@hotmail.com>
2018-12-04 16:28:30 +0000

    edge cases throwing errors for non-integer arguments

--- src/account.rb
+++ src/account.rb
@@ -11,15 +11,23 @@ class Account
   end
 
   def deposit(amount)
-    transaction = Transaction.new(amount, @balance)
-    @balance = transaction.balance
-    @statement.new_row([Time.now.strftime('%H:%M, %e %^b %Y'), amount, 0, @balance])
+    if amount.is_a? Integer
+      transaction = Transaction.new(amount, @balance)
+      @balance = transaction.balance
+      @statement.new_row([Time.now.strftime('%H:%M, %e %^b %Y'), amount, 0, @balance])
+    else
+      fail "Sorry, you cannot deposit '#{amount}'.\nPlease try an integer."
+    end
   end
 
   def withdraw(amount)
-    transaction = Transaction.new(-amount, @balance)
-    @balance = transaction.balance
-    @statement.new_row([Time.now.strftime('%H:%M, %e %^b %Y'), 0, amount, @balance])
+    if amount.is_a? Integer
+      transaction = Transaction.new(-amount, @balance)
+      @balance = transaction.balance
+      @statement.new_row([Time.now.strftime('%H:%M, %e %^b %Y'), 0, amount, @balance])
+    else
+      fail "Sorry, you cannot withdraw '#{amount}'.\nPlease try an integer."
+    end
   end
 
   def print_statement
