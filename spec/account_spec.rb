b307050690047eb2d321fa7ed77b8c57646335fc
Bill Muxworthy <bill_muxworthy@hotmail.com>
2018-12-05 14:57:35 +0000

    writes statement print test and passes

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -20,4 +20,17 @@ describe Account do
     subject.deposit(500)
     expect { subject.withdraw(600) }.to raise_error('Insufficient funds!')
   end
+
+  it 'can print a full bank statement' do
+    subject.deposit(500)
+    subject.withdraw(50)
+    expect { subject.print_statement }.to output(
+      "+--------------------+-----+-----+---------+
+| Date               | In  | Out | Balance |
++--------------------+-----+-----+---------+
+| #{Time.now.strftime('%H:%M, %e %^b %Y')} | 500 | 0   | 500     |
+| #{Time.now.strftime('%H:%M, %e %^b %Y')} | 0   | 50  | 450     |
++--------------------+-----+-----+---------+"
+    ).to_stdout
+  end
 end
