0c31c7f756747445b7ca7901204ad0b1b04aa0f1
Bill Muxworthy <bill_muxworthy@hotmail.com>
2018-12-06 14:05:29 +0000

    refactors last test with time variable

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -24,12 +24,13 @@ describe Account do
   it 'can print a full bank statement' do
     subject.deposit(500)
     subject.withdraw(50)
+    time = Time.now.strftime('%H:%M, %e %^b %Y')
     expect { subject.print_statement }.to output(
       "+--------------------+-----+-----+---------+
 | Date               | In  | Out | Balance |
 +--------------------+-----+-----+---------+
-| #{Time.now.strftime('%H:%M, %e %^b %Y')} | 500 | 0   | 500     |
-| #{Time.now.strftime('%H:%M, %e %^b %Y')} | 0   | 50  | 450     |
+| #{time} | 500 | 0   | 500     |
+| #{time} | 0   | 50  | 450     |
 +--------------------+-----+-----+---------+"
     ).to_stdout
   end
