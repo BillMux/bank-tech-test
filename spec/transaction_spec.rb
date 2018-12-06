c87f9e6fed34300dc4fb4a24c4e67630769826f5
Bill Muxworthy <bill_muxworthy@hotmail.com>
2018-12-06 16:40:58 +0000

    replaces describe blocks to context in spec

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -4,13 +4,13 @@ describe Transaction do
   let(:transaction_1) { Transaction.new(100, 0) }
   let(:transaction_2) { Transaction.new(-100, 1000) }
 
-  describe 'after deposit' do
+  context 'after deposit' do
     it 'returns amount to account' do
       expect(transaction_1.balance).to eq 100
     end
   end
 
-  describe 'after withdrawal' do
+  context 'after withdrawal' do
     it 'returns amount to account' do
       expect(transaction_2.balance).to eq 900
     end
