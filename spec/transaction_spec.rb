f5ac6bd3436402d99990c7944a2f6588713b4bc4
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-04-12 11:51:37 +0100

    Refactor, using described_class

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,18 +1,14 @@
 require_relative '../lib/transaction'
 
 describe Transaction do
-  let(:transaction_1) { Transaction.new(100, 0) }
-  let(:transaction_2) { Transaction.new(-100, 1000) }
+  let(:deposit) { described_class.new(100, 0) }
+  let(:withdraw) { described_class.new(-100, 1000) }
 
-  context 'after deposit' do
-    it 'returns amount to account' do
-      expect(transaction_1.balance).to eq 100
-    end
+  it 'returns amount to account after desposit' do
+    expect(deposit.balance).to eq 100
   end
 
-  context 'after withdrawal' do
-    it 'returns amount to account' do
-      expect(transaction_2.balance).to eq 900
-    end
+  it 'returns amount to account after withdraw' do
+    expect(withdraw.balance).to eq 900
   end
 end
